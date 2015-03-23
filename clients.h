a6ff5db27d09ba57f92531c13901935716e1ac1a
JoaoFCosta <dinojoaocosta@gmail.com>
2015-03-23 22:37:23 +0000

    Update clients.h and add comments

--- clients.h
+++ clients.h
@@ -6,10 +6,15 @@ typedef struct trieNode {
     struct trieNode * parent;
 } trieNode;
 
+/* Initiate clients structure */
 void clientsInit();
+/* Insert client into the structure */
 int clientInsert(char *);
-static trieNode * createNode(char);
-int clientsSearch(char *);
+/* Auxiliar function to search for a client */
 static int clientInsertAux(trieNode *, char *);
+/* Create a trie node with the char passed */
+static trieNode * createNode(char);
+/* Search for a client code */
+int clientsSearch(char *);
+/* Remove a client from the structure */
 int clientRemove (char *);
-/* void TrieRemove(trieNode_t **root, char *key); */
