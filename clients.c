d9f176ab6ece284f0ca39bb5a29b048d120a5dff
JoaoFCosta <dinojoaocosta@gmail.com>
2015-03-25 19:32:12 +0000

    Fix the clone function

--- clients.c
+++ clients.c
@@ -8,12 +8,12 @@
 static int clientInsertAux (trieNode *, char *);
 /* Create a trie node given a char */
 static trieNode * createNode (char);
-static void cloneAux (int *, trieNode *, trieNode *);
+static trieNode * cloneAux (int *, trieNode *, char);
 
 /* Clients structure to save clients who didnt bought anything */
 static trieNode * cheapClients = NULL;
 /* Clients Trie */
-static trieNode * clients;
+static trieNode * clients = NULL;
 
 /****************************************************/
 
@@ -27,6 +27,8 @@ void clientsInit()
 /* Insert a client in the trie */
 int clientInsert(char * client)
 {
+  if ( clients == NULL ) return -1;
+
   /* Check if two initials are uppercase */
   int initials = isupper(client[0]) && isupper(client[1]);
   /* Check if the last three characters are numbers */
@@ -142,7 +144,7 @@ trieNode * createNode(char clientChar)
 /* Search for a client code */
 int clientsSearch(char * client)
 {
-  trieNode * aux = clients;
+  trieNode * aux = cheapClients;
 
   while(1)
   {
@@ -152,9 +154,10 @@ int clientsSearch(char * client)
     if (*client == '\0') return 1;
 
     /* Search for the next char */
-    for (; aux && !found; aux = aux->next)
+    for (; aux && !found; aux = aux->next) {
       if (aux->value == *client) found = aux;
-
+    }
+      
     /* The current char was not found, string not found */
     if (found == NULL) return 0;
 
@@ -269,44 +272,73 @@ CList * clientSearchByInit (char init) {
 
 /* Clone the clients catalogue to the catalogue of clients who bought nothing */
 void clientsClone (){
-  trieNode * cat;                 /* Top level pointer */
-  trieNode * desc;                /* Descender pointer */
+  trieNode * cat;                           /* Top level pointer */
+  trieNode * desc, * aux1, * aux2, * aux3;  /* Descender pointer */
+  /* Pointers for the different levels of the catalogue */
   trieNode * lv1, * lv2, * lv3, * lv4, * lv5;
   int st1 = 0, st2 = 0, st3 = 0, st4 = 0, st5 = 0;
 
   for (lv1 = clients; lv1; lv1 = lv1->next)
   {
     st2 = 0;
-    st3 = 0;
-    st4 = 0;
-    st5 = 0;
+    aux1 = NULL; 
 
-    if ( !(st1) ) {
-      cheapClients = createNode(clients->value);
+    if ( st1 == 0 ) {
+      cheapClients = createNode(lv1->value);
       cat = cheapClients;
       st1 = 1;
     } else {
-      cloneAux( &st1, cat, lv1 );
+      cat = cloneAux( &st1, cat, lv1->value );
     }
 
     desc = cat;
 
     for (lv2 = lv1->children; lv2; lv2 = lv2->next)
     {
-      cloneAux( &st2, desc, lv2 );
+      st3 = 0;
+      aux2 = NULL;
+
+      if ( aux1 != NULL) desc = aux1;
+      desc = cloneAux( &st2, desc, lv2->value );
+      aux1 = desc;
 
       for (lv3 = lv2->children; lv3; lv3 = lv3->next)
       {
-        cloneAux( &st3, desc, lv3 );
+        st4 = 0;
+        aux3 = NULL;
+
+        if ( aux2 != NULL ) desc = aux2;
+        desc = cloneAux( &st3, desc, lv3->value );
+        aux2 = desc;
 
         for (lv4 = lv3->children; lv4; lv4 = lv4->next)
         {
-          cloneAux( &st4, desc, lv4 );
+          st5 = 0;
+
+          if ( aux3 != NULL ) desc = aux3;
+          desc = cloneAux( &st4, desc, lv4->value );
+          aux3 = desc;
 
           for (lv5 = lv4->children; lv5; lv5 = lv5->next)
           {
-            cloneAux( &st5, desc, lv5 );
+            desc = cloneAux( &st5, desc, lv5->value );
           }
+        }
+      }
+    }
+  }
+
+  /* Print the catalogue */
+  for (cat = cheapClients; cat; cat = cat->next){
+    printf("%c\n", cat->value);
+    for (aux1 =cat->children; aux1; aux1 = aux1->next){
+      printf("  %c\n", aux1->value);
+      for (aux2 = aux1->children; aux2; aux2 = aux2->next){
+        printf("    %c\n", aux2->value);
+        for (aux3 = aux2->children; aux3; aux3 = aux3->next) {
+          printf("       %c\n", aux3->value);
+          for (desc = aux3->children; desc; desc = desc->next)
+            printf("        %c\n", desc->value);
         }
       }
     }
@@ -314,18 +346,18 @@ void clientsClone (){
 }
 
 /* Auxiliar function to help creating the clone catalog nodes */
-void cloneAux ( int * status, trieNode * node, trieNode * originnode ) {
-  if ( *status )
+trieNode * cloneAux ( int * status, trieNode * node, char c ) {
+  if ( (*status) == 0 )
   {
     /* First time at the given level */
-    node->children = createNode(originnode->value);
-    (node->children)->parent = node->parent;
-    node = node->children;
-    *status = 1;
+    node->children = createNode(c);
+    (node->children)->parent = node;
+    (*status) = 1;
+    return (node->children);
   } else {
-    node->next = createNode(originnode->value);
+    node->next = createNode(c);
     (node->next)->prev = node;
     (node->next)->parent = node->parent;
-    node = node->next;
+    return (node->next);
   }
 }
