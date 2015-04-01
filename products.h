8614c5b34011163186708f204b25260938a772be
Leandro Salgado <d-x@live.com.pt>
2015-04-01 18:57:31 +0000

    Update products.h

--- products.h
+++ products.h
@@ -1,8 +1,27 @@
 #ifndef trie_h 
 #define trie_h
 
+#define ALPHA 26
+
+typedef struct node{
+	char code[6];
+	struct node* link[ALPHA];
+	int* back;
+} Product;
+
+typedef struct listnode{
+	char code[6];
+	struct listnode * next;
+} PList;
+
+Product* new_product();
+
 int insert_product(char key[]);
 
 int search(char key[]);
+
+PList* searchI(char c);
+
+void removeP(char key[]);
 
 #endif
