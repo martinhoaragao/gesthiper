bc9a69932ba042bcb21f9859a30c6f39d5379308
Martinho Aragão <martinhoaragao@gmail.com>
2015-04-05 23:16:06 +0100

    Enhance comments, and organisation of code

--- accounting.h
+++ accounting.h
@@ -23,11 +23,20 @@ typedef struct {
   ProductNode * monthAccounting[12];
 } Accounting;
 
+/*-------------------------------API-------------------------------*/
 
 /* Allocates and initializes an array of ProductNode to NULL */
 Accounting initAccounting();
 int insertAccounting(Accounting bills, Tokens *);
+
+/* Removes a sale by ProductCode */
 int removeAccounting(Accounting bills, char *);
+
+/* 
+ * Searches the product through the different trees 
+ * Returns 0 (FALSE) if not found or 1 (TRUE) if found
+ */
 bool searchAccounting(Accounting, char *);
+
 /* Finds a node and returns the sales of a specific month by promotion or normal */
 double * getMonthlySales(Accounting bills, int, char *);
