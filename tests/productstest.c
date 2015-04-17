4a68097ec8c33367ffdf5eb879a0716c374dead5
Leandro Salgado <d-x@live.com.pt>
2015-04-17 14:00:48 +0100

    Update productstest.c

--- tests/productstest.c
+++ tests/productstest.c
@@ -4,7 +4,7 @@
 #include <string.h>
 
 int main(){
-	char filename[100], prod[6], key[6], buffer[32], letra, keys;
+	char filename[100], prod[6], key[6], letra, keys;
 	int qnt = 0, index = 0, validPCodes = 0, i = 0;
 	int n = 0, x = 30, max, garbage;
 	ProductsCat* cat = initProductsCat();
@@ -16,14 +16,13 @@ int main(){
 	printf("Qual o nome do ficheiro a ler?\n");
 	garbage = scanf("%s", filename);
 	
-	fr = fopen("FichProdutos.txt", "r");
+	fr = fopen(filename, "r");
 	if (fr == NULL){
 		printf("O ficheiro não existe!\n");
 		return 1;
 	}
 
-	while(fgets(buffer, 32, fr)){
-		strcpy(key, buffer);
+	while(fgets(key, 10, fr)){
 		validPCodes += insert_product(cat, key);
 		if(i<194000) search2(cat, key);
 		i++;
@@ -60,100 +59,26 @@ int main(){
 	p = searchI(cat, letra);
 	max = getQnt(p);
 
+	i=0;
 	while(1){
-		printf("A mostrar resultados %d-%d de %d.\n", (n+1), (n+31), max);
+		if(i==1)printf("A mostrar resultados %d-%d de %d.\n", (n+1), (n+31), max);
 		for(; n < x; n++)
 			printf("%d. %s\n", (n+1), getCode(p, n));
 
 		printf("S pra seguinte, A para anterior, Q para sair\n");
 		garbage = scanf(" %c", &keys);
 		if(keys=='Q' || keys=='q') break;
-		else if(keys=='S' || keys=='s') x += 30;
+		else if(keys=='S' || keys=='s'){
+			i = 1;
+			if((x+30) > max) x = max;
+			else x += 30; 
+		}
 		else if(keys=='A' || keys=='a'){
+			i = 1;
 			if(n >= 60) {n-=60; x-=30;}
 			else printf("error\n");
 		}else printf("Escolha inválida\n");
 	} 
 	garbage = 0;
 	return garbage;
-}
-#include "products.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(){
-	char filename[100];
-	char prod[6];
-	char letra;
-	ProductsCat* cat = initProductsCat();
-	ProductsCat* catclone;
-	PList *lista;
-	int noscope = 0; int quack = 0;
-	printf("Qual o nome do ficheiro a ler?\n");
-	//scanf("%s", filename);
-	
-	FILE *fr = fopen("FichProdutos.txt", "r");
-	if (fr == NULL){
-		printf("O ficheiro não existe!\n");
-		return 1;
-	}
-
-	int validPCodes = 0, i=0, length;
-	char key[6];
-	while(fgets(key, 32, fr)){
-		length = strlen(key); 
-		validPCodes += insert_product(cat, key);
-		if(i<194000) search2(cat, key);
-		i++;
-	}
-	printf("O ficheiro %s foi lido.\n", filename);
-	printf("Foram lidas %d linhas.\n", i);
-	printf("Foram validadas %d linhas.\n", validPCodes);
-
-
-	printf("Qual o produto que quer procurar?\n");
-	scanf("%s", prod);
-	printf("%d\n", search(cat, prod));
-
-
-	printf("Remove code\n");
-	scanf("%s", prod);
-	if(search(cat, prod)){
-		catclone = removeProduct(cat, prod);
-	}else printf("error\n");
-
-
-	PList *xs = notboughtProducts(cat);
-	noscope=getQnt(xs);
-	printf("%d\n", noscope);
-	for(quack=0; quack<noscope; quack++){
-		printf("%s\n", getCode(xs, quack));
-	}
-
-
-	/* Querie 2 */
-	printf("Querie 2\nQual a inicial pela qual quer procurar?\n");
-	scanf(" %c", &letra);
-
-	int n = 0, x = 30, max; char keys;
-	PList *p = searchI(cat, letra);
-	max = getQnt(p);
-
-	while(1){
-		printf("A mostrar resultados %d-%d de %d.\n", (n+1), (n+31), max);
-		for(; n < x; n++)
-			printf("%d. %s\n", (n+1), getCode(p, n));
-
-		printf("S pra seguinte, A para anterior, Q para sair\n");
-		scanf(" %c", &keys);
-		if(keys=='Q' || keys=='q') break;
-		else if(keys=='S' || keys=='s') x += 30;
-		else if(keys=='A' || keys=='a'){
-			if(n >= 60) {n-=60; x-=30;}
-			else printf("error\n");
-		}else printf("Escolha inválida\n");
-	} 
-
-	return 0;
 }
