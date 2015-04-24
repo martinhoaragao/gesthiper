5b50900db5a96735d538461e7e9b8961333acc72
Leandro Salgado <d-x@live.com.pt>
2015-04-24 12:49:09 +0100

    Update salesp.c

--- salesp.c
+++ salesp.c
@@ -1,7 +1,7 @@
 /* AVLP -> AVL of products, it's the main AVL
  * AVLPC -> AVL of clients used by AVLP of products
  */
-
+#define _GNU_SOURCE 1 /* it's needed to use strdup */
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
